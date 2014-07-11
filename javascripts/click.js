5b1d8613f786723edc31670451089bf98286f683
yanglf2 <yanglf2@lenovo.com>
2014-07-11 12:09:27 +0800

    finish navbar

---
+++ javascripts/click.js
@@ -0,0 +1,8 @@
+(function() {
+    var c = document.createElement('script'); 
+    c.type = 'text/javascript';
+    c.async = true;
+    c.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'www.clicki.cn/boot/45708';
+    var h = document.getElementsByTagName('script')[0];
+    h.parentNode.insertBefore(c, h);
+})();
